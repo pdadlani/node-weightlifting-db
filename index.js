28599d8d1ee0bfc6e08b40b60aff71ab367e2166
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-10-18 09:28:43 -0700

    setting up heroku.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const server = require('./api/server');
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => {
   console.log(`\n*** Server running on port ${PORT} ***\n`);
